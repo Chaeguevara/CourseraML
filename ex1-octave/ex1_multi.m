bd0fb5d2b44d36e4b85041e5503ea3db8a737e7a
Chaeguevara <dfso2@naver.com>
2021-07-28 21:59:45 +0900

    HW1 Done

--- ex1-octave/ex1_multi.m
+++ ex1-octave/ex1_multi.m
@@ -118,8 +118,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+price = X*theta; % You should change this
 
 % ============================================================
 
@@ -163,7 +162,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = normalEqn(X,y); % You should change this
 
 
 % ============================================================
--- ex1-octave/normalEqn.m
+++ ex1-octave/normalEqn.m
@@ -13,15 +13,7 @@ theta = zeros(size(X, 2), 1);
 %
 
 % ---------------------- Sample Solution ----------------------
-numIter = 200;
-alpha = 0.1;
-X1 = X(:,1); % ones
-X2 = X(:,2:size(X,2)); % features to be normalized
-[X2, mu, sigma] = featureNormalize(X);
-Xnew = [X1 X2];
-J = 0;
-[Xnew J] = gradientDescentMulti(Xnew, y, theta, alpha, numIter);
-
+theta = inv(X'*X)*X'*y;
 
 
 % -------------------------------------------------------------
